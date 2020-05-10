af3be96acf024fa0497bd0aff54c50e97cc8074c
tnwong <wong.tsznok@gmail.com>
2020-05-10 10:30:46 +0800

    new project init

---
+++ iOSRecruitmentTest1/iOSRecruitmentTest1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSRecruitmentTest1
+//
+//  Created by Tsznok Wong on 10/5/2020.
+//  Copyright © 2020 USThing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
